4bcd8569c333ab1a33a71054337aa460db4002de
Nathan Guzman <nate18@utexas.edu>
2022-04-22 10:55:54 -0500

    Added camera to upload screen

--- navigation/screens/UploadScreen.js
+++ navigation/screens/UploadScreen.js
@@ -1,14 +1,70 @@
 import * as React from 'react';
 import {View, Text} from 'react-native';
+import { useRef, useEffect, useState } from 'react';
+
 
 export default function UploadScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text
-        onPress={() => navigation.navigate('SnapShare')}
-        style={{fontSize: 26, fontWeight: 'bold'}}>
-        Upload Screen
-      </Text>
-    </View>
-  );
+	const videoRef = useRef(null);
+	const photoRef = useRef(null);
+
+	const [hasPhoto, setHasPhoto] = useState(false);
+
+	const getVideo = () => {
+		navigator.mediaDevices
+			.getUserMedia({
+				video: {width: 1920, height: 1080} 
+			})
+			.then(stream => {
+				let video = videoRef.current;
+				video.srcObject = stream;
+				video.play();
+			})
+			.catch( err => {
+				console.error(err);
+			})
+	}
+
+	const takePhoto = () => {
+		const width = 414;
+		const height = width / (16/9);
+
+		let video = videoRef.current;
+		let photo = photoRef.current;
+
+		photo.width = width;
+		photo.height = height;
+
+		let ctx = photo.getContext('2d');
+		ctx.drawImage(video, 0, 0, width, height);
+		setHasPhoto(true);
+	}
+
+	const closePhoto = () => {
+		let photo = photoRef.current;
+		let ctx = photo.getContext('2d');
+
+		ctx.clearRect(0, 0, photo.width, photo,height);
+
+		setHasPhoto(false);
+
+	}
+
+	useEffect(() => {
+		getVideo();
+	}, [videoRef])
+
+	return (
+		<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+			<div className="UploadScreen">
+				<div className="camera">
+					<video ref={videoRef} style={{flex: 1, alignItems: 'center', justifyContent: 'center', width: '30%', height: '30%'}}></video>
+					<button onClick = {takePhoto}>SNAP!</button>
+				</div> 
+				<div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+					<canvas ref={photoRef}></canvas>
+					<button>CLOSE!</button>
+				</div>
+			</div>
+		</View>
+	);
 }
