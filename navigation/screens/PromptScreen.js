30eb9be1d2194aa5d85ae2612f2b211538042285
jasmine-y-wang <jasminewang020@gmail.com>
2022-04-30 15:21:03 -0500

    added textinput

--- navigation/screens/PromptScreen.js
+++ navigation/screens/PromptScreen.js
@@ -1,16 +1,33 @@
 import * as React from 'react';
-import {View, Text, ScrollView, Image, Button} from 'react-native';
+import {View, Text, ScrollView, Image, Button, TextInput, StyleSheet} from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function PromptScreen({navigation}) {
+  const [text, onChangeText] = React.useState(null);
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', padding: 70}}>
         <Text style={{fontSize: 48, color: '#535865'}}>Prompt</Text>
         <Text style={{fontSize: 48, color: '#535865'}}> Suggestions</Text>
 
+        <TextInput 
+          style={styles.input} 
+          onChangeText={onChangeText} 
+          value={text} 
+          placeholder="Enter a prompt suggestion!"
+        />
       </View>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
